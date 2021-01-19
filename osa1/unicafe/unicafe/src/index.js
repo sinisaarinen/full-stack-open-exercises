48ac278d1801cdc9efbdc7cf91efc07bae268edf
sinisaarinen <sini.saarinen@hotmail.fi>
2021-01-19 20:08:16 +0200

    added exercise 1.7

--- osa1/unicafe/unicafe/src/index.js
+++ osa1/unicafe/unicafe/src/index.js
@@ -40,6 +40,10 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good+neutral+bad}</p>
+      <p>average {(good-bad)/(good+neutral+bad) || 0}</p>
+      <p>positive {100*good/(good+neutral+bad) || 0} %</p>
+
     </div>
   )
 }
