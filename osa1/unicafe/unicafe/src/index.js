234ebfb560725b2a5f54a65169a1fd316e8cd404
sinisaarinen <sini.saarinen@hotmail.fi>
2021-01-19 20:48:07 +0200

    added exercise 1.10

--- osa1/unicafe/unicafe/src/index.js
+++ osa1/unicafe/unicafe/src/index.js
@@ -1,21 +1,29 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const StatisticLine = ({text, value}) => {
+  return (
+    <div>
+      {text} {value}
+    </div>
+  )
+  }
+
 const Statistics = ({good, neutral, bad}) => {
   if (good === 0 && neutral === 0 && bad === 0) {
     return (
       <p>No feedback given</p>
     )
   }
- return (
-   <>
-  <p>good {good}</p>
-  <p>neutral {neutral}</p>
-  <p>bad {bad}</p>
-  <p>all {good+neutral+bad}</p>
-  <p>average {(good-bad)/(good+neutral+bad) || 0}</p>
-  <p>positive {100*good/(good+neutral+bad) || 0} %</p>
-  </>
+  return(
+    <div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={good+neutral+bad} />
+      <StatisticLine text="average" value={(good-bad)/(good+neutral+bad) || 0} />
+      <StatisticLine text="positive" value={`${100*good/(good+neutral+bad) || 0}%`} />
+    </div>
  )
 }
 
@@ -29,7 +37,6 @@ const Button = (props) => {
   }
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
